1b4736e31473726afa46c77b7e8180406719741b
Mohammed Nawfal <nawfal.mohammed006@gmail.com>
2026-01-05 17:07:08 +0530

    Update recipeRouter import path in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/dbConfig.js";
-import recipeRouter from "./views/recipeRoutes.js";
+import recipeRouter from "./routes/recipeRoutes.js";
 
 dotenv.config();
 
@@ -23,3 +23,4 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`server starts at ${port}`)
 });
+
